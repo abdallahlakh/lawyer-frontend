bc605114e26b5653333484ee1755cb43be5fa4b4
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-26 14:11:34 +0100

    this is

--- src/advanced-search/AboutUs.js
+++ src/advanced-search/AboutUs.js
@@ -6,7 +6,7 @@ const AboutUs = () => {
     <h1 className="text-4xl font-bold mb-4 text-black">About Us</h1>
     <p className="text-lg text-black mb-4">We are a team of passionate individuals dedicated to creating the best user experience possible. Our team is composed of a diverse group of individuals with a wide range of skills and expertise. We strive to maintain a high standard of quality in our work and to deliver top-notch solutions to our clients.</p>
     <h2 className="text-2xl font-bold mb-2 text-black">Contact Us</h2>
-    <p className="text-lg text-black mb-4">You can reach us at a_lakhdari@estin.dz or call us at(+213) 0554330409.</p>
+    <p className="text-lg text-black mb-4">You can reach us at a_lakhdari@estin.dz or call us </p>
 </div>
     );
 }
