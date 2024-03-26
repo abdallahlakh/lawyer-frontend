c1b54dc59d96112a5c48104ad2b88d0f06f82f65
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-26 14:25:26 +0100

    this is threeee

--- src/advanced-search/HeaderSection.js
+++ src/advanced-search/HeaderSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchSection = () => {
     return (
         <div  style={{backgroundColor: "#006400"}} className="bg-blue-500 text-white py-8 text-center">
-            <h1 className="text-4xl font-semibold mb-4">Mouhami DZ</h1>
+            <h1 className="text-4xl font-semibold mb-4">Mouhami DhahahaZ</h1>
             <h2 className="text-xl">Where You Make Booking,Review,and more ...</h2>
         </div>
     );
