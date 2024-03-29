9d6e1d8a55056348b0375276aff1595a94eb6469
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-03-29 18:59:38 +0100

    Update Navbar.js

--- src/advanced-search/Navbar.js
+++ src/advanced-search/Navbar.js
@@ -130,9 +130,9 @@ const Navbar = () => {
     <button onClick={toggleNavbar} className="fixed right-0 top-0 m-4 p-2 bg-gray-700 text-white rounded">☰</button>
     {isNavbarVisible && (
         
-        <div className="fixed right-0 top-0 h-screen flex flex-col justify-between items-center p-4 bg-gray-700 z-50 transition-all duration-500 ease-in-out">
-         <button onClick={toggleNavbar} className="fixed right-0 top-0 m-4 p-2 bg-gray-700 text-white rounded">☰</button>
-   
+       <div className="fixed right-0 top-0 h-screen flex flex-col justify-between items-center p-4 bg-gray-700 z-50 transition-all duration-500 ease-in-out">
+             <button onClick={toggleNavbar} className="absolute top-0 right-0 m-4 p-2 bg-gray-700 text-white rounded">☰</button>
+
             <div className="text-white mb-8">
                 {userType === 'customer' && <Link to="/my-account" className="text-white">Customer account</Link>}
                 {userType === 'lawyer' && <Link to="/my-account" className="text-white">Lawyer account</Link>}
