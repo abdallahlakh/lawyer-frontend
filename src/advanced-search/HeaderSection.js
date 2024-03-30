3d6e37cb79b275800459f0dba8372432af2040ef
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-30 21:53:00 +0100

    this is 58

--- src/advanced-search/HeaderSection.js
+++ src/advanced-search/HeaderSection.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+const headerStyles = {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Transparent black background
+    color: '#fff', // White text color
+    padding: '20px', // Padding around content
+    borderRadius: '8px', // Rounded corners
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Shadow for a modern look
+};
 const SearchSection = () => {
     return (
+        <div className="header-section" style={headerStyles}>
         <div  className="bg-gray-800 text-white py-8 text-center">
     <h1 className="text-4xl font-semibold mb-4">Mouhami DZ</h1>
     <h2 className="text-xl">Where You Make Booking, Review, and more ...</h2>
         </div>
-
+</div>
     );
 };
 
