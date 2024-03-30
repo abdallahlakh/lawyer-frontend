ec5c8e7db5faab3e65f42882d04167b8ae18123a
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-03-30 04:56:48 +0100

    Update HeaderSection.js

--- src/advanced-search/HeaderSection.js
+++ src/advanced-search/HeaderSection.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const SearchSection = () => {
     return (
-        <div  style={{backgroundColor: "#006400"}} className="bg-blue-500 text-white py-8 text-center">
-            <h1 className="text-4xl font-semibold mb-4">Mouhami DZ</h1>
-            <h2 className="text-xl">Where You Make Booking,Review,and more ...</h2>
+        <div  className="bg-gray-800 text-white py-8 text-center">
+    <h1 className="text-4xl font-semibold mb-4">Mouhami DZ</h1>
+    <h2 className="text-xl">Where You Make Booking, Review, and more ...</h2>
         </div>
+
     );
 };
 
