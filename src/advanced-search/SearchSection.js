51da8395141c48feedcef5825400adecfe9989de
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-03-30 05:33:58 +0100

    Update SearchSection.js

--- src/advanced-search/SearchSection.js
+++ src/advanced-search/SearchSection.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchSection = () => {
     return (
-        <div style={{backgroundColor: "#006400"}} className="bg-blue-500 text-white py-8 text-center">
+        <div style={{backgroundColor: "#006400"}} className="bg-gradient-to-r from-green-200 to-green-100 text-white py-8 text-center">
             <h1 className="text-4xl font-semibold mb-4">Here you can search for lawyers</h1>
-            <h2 className="text-xl">use this Filters to Find the Lawyer that you want</h2>
+            <h2 className="text-xl">Use these filters to find the lawyer you want</h2>
         </div>
     );
 };
