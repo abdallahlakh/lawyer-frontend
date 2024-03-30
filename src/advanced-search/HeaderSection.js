71278a522da3bdf040095ce39bce552fcdcd17dd
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-30 21:57:04 +0100

    this is 59

--- src/advanced-search/HeaderSection.js
+++ src/advanced-search/HeaderSection.js
@@ -11,7 +11,7 @@ const headerStyles = {
 const SearchSection = () => {
     return (
         <div className="header-section" style={headerStyles}>
-            <div className="bg-gray-800 text-white py-8 text-center">
+            <div className="text-white py-8 text-center">
                 <h1 className="text-4xl font-semibold mb-4">Mouhami DZ</h1>
                 <h2 className="text-xl">Where You Make Booking, Review, and more ...</h2>
             </div>
