c398eaa3200dd7cfe0968dd0caccb50a28de82fd
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-03-30 05:42:37 +0100

    Update SeeBooking.js

--- src/advanced-search/SeeBooking.js
+++ src/advanced-search/SeeBooking.js
@@ -93,58 +93,59 @@ const SeeBooking = () => {
         fetchBookings();
     }, [id]);
     
-    return (<div className="container mx-auto px-4 py-8">
-    <div className="flex items-center justify-between mb-6">
-        <h2 className="text-3xl font-bold text-black">Your Bookings</h2>
-        <button onClick={() => navigate(-1)} className="bg-red-600 hover:bg-red-400 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:scale-105 shadow-lg">Back</button>
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {bookings.length > 0 ? (
-            bookings.map(booking => (
-                <div key={booking.id} className="bg-white rounded-lg overflow-hidden shadow-lg relative">
-                    <button 
-                        onClick={() => deleteBooking(booking.id)} 
-                        className="absolute top-0 right-0 m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded transition duration-200 ease-in-out transform hover:scale-105 shadow-lg"
-                    >
-                        X
-                    </button>
-                    <div className="p-6">
-                        <h3 className="text-xl font-semibold mb-2 text-black">Booking ID: {booking.id}</h3>
-                        <p className="text-black">Date: {booking.date}</p>
-                        <p className="text-black">Time From: {booking.time_from}</p>
-                        <p className="text-black">Time To: {booking.time_to}</p>
-                        <p className="text-black">Lawyer: {booking.lawyer}</p>
-                        <p className="text-black">State: {booking.state}</p>
-                    </div>
-                    <div className="p-6 flex justify-center space-x-4 items-center">
-                        <Link 
-                            to={`/lawyer-details/${booking.lawyer}`} 
-                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-md transition duration-200 ease-in-out transform hover:scale-105"
-                        >
-                            See Lawyer
-                        </Link>
-                        <select 
-                            id={`state-${booking.id}`} 
-                            className="border-2 border-black-500 text-black-700 bg-gray-100 hover:border-black-700 focus:border-white-700 h-10 pl-5 pr-10 hover:bg-gray-200 focus:outline-none appearance-none shadow-md cursor-pointer"
-                        >
-                            <option value="" disabled selected>Update the state</option>
-                            <option value="REQUESTED" className="py-1 text-black-700">Requested</option>
-                            <option value="CANCELLED" className="py-1 text-black-700">Cancelled</option>
-                        </select>
-                        <button 
-                            onClick={() => updateBookingState(booking.id)} 
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md transition duration-200 ease-in-out transform hover:scale-105"
-                        >
-                            Update State
-                        </button>
-                    </div>
-                </div>
-            ))
-        ) : (
-            <p className="text-black text-center">You have no bookings.</p>
-        )}
-    </div>
-</div>
+    return (
+          <div className="container mx-auto px-4 py-8">
+            <div className="flex items-center justify-between mb-6">
+                <h2 className="text-3xl font-bold text-gray-900">Your Bookings</h2>
+                <button onClick={() => navigate(-1)} className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:scale-105 shadow-lg">Back</button>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {bookings.length > 0 ? (
+                    bookings.map(booking => (
+                        <div key={booking.id} className="bg-gray-200 rounded-lg overflow-hidden shadow-lg relative">
+                            <button 
+                                onClick={() => deleteBooking(booking.id)} 
+                                className="absolute top-0 right-0 m-2 bg-gray-600 hover:bg-gray-400 text-white font-bold py-1 px-2 rounded transition duration-200 ease-in-out transform hover:scale-105 shadow-lg"
+                            >
+                                X
+                            </button>
+                            <div className="p-6">
+                                <h3 className="text-xl font-semibold mb-2 text-gray-900">Booking ID: {booking.id}</h3>
+                                <p className="text-gray-900">Date: {booking.date}</p>
+                                <p className="text-gray-900">Time From: {booking.time_from}</p>
+                                <p className="text-gray-900">Time To: {booking.time_to}</p>
+                                <p className="text-gray-900">Lawyer: {booking.lawyer}</p>
+                                <p className="text-gray-900">State: {booking.state}</p>
+                            </div>
+                            <div className="p-6 flex justify-center space-x-4 items-center">
+                                <Link 
+                                    to={`/lawyer-details/${booking.lawyer}`} 
+                                    className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded-md transition duration-200 ease-in-out transform hover:scale-105"
+                                >
+                                    See Lawyer
+                                </Link>
+                                <select 
+                                    id={`state-${booking.id}`} 
+                                    className="border-2 border-gray-900 text-gray-900 bg-white hover:border-gray-700 focus:border-gray-700 h-10 pl-5 pr-10 hover:bg-gray-200 focus:outline-none appearance-none shadow-md cursor-pointer"
+                                >
+                                    <option value="" disabled selected>Update the state</option>
+                                    <option value="REQUESTED" className="py-1 text-gray-900">Requested</option>
+                                    <option value="CANCELLED" className="py-1 text-gray-900">Cancelled</option>
+                                </select>
+                                <button 
+                                    onClick={() => updateBookingState(booking.id)} 
+                                    className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded-md transition duration-200 ease-in-out transform hover:scale-105"
+                                >
+                                    Update State
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-gray-900 text-center">You have no bookings.</p>
+                )}
+            </div>
+        </div>
     );
 };
 
